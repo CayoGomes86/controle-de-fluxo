5f7845da56b9a650675645c7ffcb26cd97f00d09
Cayo Gomes <cayogomes.git@gmail.com>
2026-04-08 10:33:57 -0300

    Aprimorado com um laço For

--- src/ExemploDoWhile.java
+++ src/ExemploDoWhile.java
@@ -2,15 +2,26 @@ import java.util.Random;
 public class ExemploDoWhile {
     public static void main(String[] args) {
         System.out.println("Discando...");
-
+        int tentativas = 0;
         do{
+            
         System.out.println("Chamando...");
-        }while(tocando());
+        
+        tentativas = tentativas + 1;
+        
+        }while(tocando() && tentativas < 5);
+        
+        if(tentativas == 5){
+            System.out.println("Encaminhado para Caixa de Mensagens");
+        }else{
+            System.out.println("Alô!!!");
 
-    System.out.println("Alô!!!");
+        }
+
+    
     }
-    private static boolean tocando(){
-        boolean atendeu = new Random().nextInt(2) == 1;
+    public static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3) == 1;
         return !atendeu;
 
     }
